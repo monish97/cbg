df2d62614a2ec65e8db32dccc8f90e74c3fe539a
monish97 <monishvyas1990@gmail.com>
2026-01-05 19:16:04 +0530

    Update fetch-games.php

--- fetch-games.php
+++ fetch-games.php
@@ -3,7 +3,12 @@ header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
 $url = 'https://gamemonetize.com/feed.php?format=0&page=1';
-$games = file_get_contents($url);
-echo $games;
 
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$games = curl_exec($ch);
+curl_close($ch);
+
+echo $games;
 ?>
