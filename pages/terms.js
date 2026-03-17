c297a131b3654dc4e8f1267cb54b27e4eacc9923
monish97 <monishvyas1990@gmail.com>
2026-03-17 17:40:26 +0530

    Update terms.js

--- pages/terms.js
+++ pages/terms.js
@@ -1,1 +1,8 @@
-
+export default function TermsOfService() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Terms of Service</h1>
+      <p>These are placeholder terms for Casual Browser Games. Full terms will be added later.</p>
+    </div>
+  );
+}
