b7f84260966b17cb7c25c4eca2ee7697038b561b
monish97 <monishvyas1990@gmail.com>
2026-03-17 17:51:09 +0530

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -1,1 +1,8 @@
-
+export default function About() {
+  return (
+    <div>
+      <h1>About Casual Browser Games</h1>
+      <p>Welcome to Casual Browser Games! This is a placeholder page for SEO purposes.</p>
+    </div>
+  );
+}
