14c9c8897fd93432eeaf9e7751a9c20a59274710
monish97 <monishvyas1990@gmail.com>
2026-03-17 17:40:42 +0530

    Update faq.js

--- pages/faq.js
+++ pages/faq.js
@@ -1,1 +1,8 @@
-
+export default function FAQ() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>FAQ</h1>
+      <p>This page will contain frequently asked questions for Casual Browser Games.</p>
+    </div>
+  );
+}
