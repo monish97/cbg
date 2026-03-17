bf472f44883b25ea34e5cc38806c983a66fab532
monish97 <monishvyas1990@gmail.com>
2026-03-17 12:21:17 +0530

    Add GamePage component to display game details

---
+++ pages/game/[slug].js
@@ -0,0 +1,29 @@
+import Head from "next/head";
+import { getGames } from "../../lib/api";
+
+export default function GamePage({ game }) {
+  if (!game) return <div>Game not found</div>;
+
+  return (
+    <>
+      <Head>
+        <title>Play {game.title} Online Free</title>
+        <meta name="description" content={game.description} />
+      </Head>
+
+      <div>
+        <h1>{game.title}</h1>
+
+        <iframe
+          src={game.iframe}
+          width="100%"
+          height="600px"
+          allowFullScreen
+          style={{ border: "none" }}
+        ></iframe>
+
+        <p style={{ marginTop: "20px" }}>{game.description}</p>
+      </div>
+    </>
+  );
+}
