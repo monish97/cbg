11bf9bcc0374658cbecb27819f1de262dd98218a
monish97 <monishvyas1990@gmail.com>
2026-03-17 22:53:55 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,25 @@
 import "../styles/globals.css";
 import Footer from "../components/Footer";
 import Layout from "../components/Layout";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
+    <>
+      <Head>
+        {/* ✅ Step 4: Title + Description */}
+        <title>Casual Browser Games - Play Free Online Games</title>
+        <meta
+          name="description"
+          content="Play free casual browser games online. No download required. Enjoy action, puzzle, racing and more!"
+        />
+
+        {/* ✅ Step 5: Keywords */}
+        <meta
+          name="keywords"
+          content="casual games, browser games, free online games, play games online"
+        />
+      </Head>
     <>     
       <Layout>
         <Component {...pageProps} />
