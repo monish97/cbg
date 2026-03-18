e62f0a613cb69912c6f8625e7899ee5ba1e9325c
monish97 <monishvyas1990@gmail.com>
2026-03-18 17:44:40 +0530

    Create categories.js

---
+++ pages/categories.js
@@ -0,0 +1,23 @@
+export const categoryMap = {
+  "All": null,           // No category parameter
+  "IO": 1,
+  "2 Player": 2,
+  "3D": 3,
+  "Action": null,        // Action uses the main feed without category param
+  "Adventure": 4,
+  "Arcade": 5,
+  "Baby": 19,
+  "Bejeweled": 6,
+  "Boys": 7,
+  "Clicker": 8,
+  "Cooking": 9,
+  "Girls": 10,
+  "Hypercasual": 11,
+  "Multiplayer": 12,
+  "Puzzle": 13,
+  "Racing": 14,
+  "Shooting": 15,
+  "Soccer": 16,
+  "Sports": 17,
+  "Stickman": 18,
+};
