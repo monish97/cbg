ce7a647ed9ab3ee07dffdd177064940f735ff9e5
monish97 <monishvyas1990@gmail.com>
2026-03-18 23:49:50 +0530

    Update GameCard.js

--- components/GameCard.js
+++ components/GameCard.js
@@ -1,12 +1,13 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function GameCard({ game }) {
   return (
-    <div className="game-card">
-      <img src={game.thumb} alt={game.title} width={game.width} height={game.height} />
-      <h3>{game.title}</h3>
-      <p>{game.description}</p>
-      <a href={game.url} target="_blank" rel="noopener noreferrer">
-        Play Game
-      </a>
+    <div className="card">
+      <Link href={game.url} target="_blank">
+        <Image src={game.thumb} alt={game.title} width={220} height={140} />
+        <h3>{game.title}</h3>
+      </Link>
     </div>
   );
 }
