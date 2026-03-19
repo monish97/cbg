9a237c4f4412850a190c396f4aaed328cfba4ae5
monish97 <monishvyas1990@gmail.com>
2026-03-19 14:12:53 +0530

    Update GameCard.js

--- components/GameCard.js
+++ components/GameCard.js
@@ -4,18 +4,18 @@ import Image from "next/image";
 export default function GameCard({ game }) {
   return (
     <div className="game-card">
-      <Link href={`/play?id=${game.url}`}>
-          <a>
-            <Image
-              src={game.thumb}
-              alt={game.title}
-              width={220}
-              height={140}
-              style={{ borderRadius: "8px 8px 0 0" }}
-            />
-            <h3>{game.title}</h3>
-          </a>
-        </Link>
+      <Link href={`/games/${game.url}`}>
+        <a>
+          <Image
+            src={game.thumb}
+            alt={game.title}
+            width={220}
+            height={140}
+            style={{ borderRadius: "8px 8px 0 0" }}
+          />
+          <h3>{game.title}</h3>
+        </a>
+      </Link>
     </div>
   );
 }
