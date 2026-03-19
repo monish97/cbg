d2604380fa96b859024a39d81beafb5866395c2c
monish97 <monishvyas1990@gmail.com>
2026-03-19 12:52:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      'img.gamemonetize.com',        // replace with your actual thumbnail domain(s)      
+    ],
+  },
+};
+
+module.exports = nextConfig;/** @type {import('next').NextConfig} */
 const nextConfig = {};
 
 module.exports = nextConfig;
