05f646d12cbf25f372e00680cf4f6ec2a5903d6f
monish97 <monishvyas1990@gmail.com>
2026-03-19 13:58:22 +0530

    Update and rename [slug].js to [id].js

--- pages/game/[id].js
+++ pages/game/[id].js
@@ -60,13 +60,13 @@ export default function GamePage({ game, relatedGames }) {
 
 // SSR
 export async function getServerSideProps(context) {
-  const gameId = parseInt(context.params.id, 10); // get id from URL
+  const gameId = Number(context.params.id); // convert string to number
   const game = gamesData.find((g) => g.id === gameId);
 
   if (!game) return { notFound: true };
 
   const relatedGames = gamesData
-    .filter((g) => g.category === game.category && g.id !== gameId)
+    .filter((g) => g.category === game.category && g.id !== game.id)
     .slice(0, 6);
 
   return { props: { game, relatedGames } };
