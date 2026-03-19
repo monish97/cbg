a39ac6e37e99deb6ff2399366c7de4a6c2ff3f34
monish97 <monishvyas1990@gmail.com>
2026-03-19 13:51:55 +0530

    Update GameCard.js

--- components/GameCard.js
+++ components/GameCard.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function GameCard({ game }) {
   return (
     <div className="game-card">
-      <Link href={`/games/${game.url}`}>
+      <Link href={`/games/${game.id}`}>
         <a>
           <Image
             src={game.thumb}
